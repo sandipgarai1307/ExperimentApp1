562c30ab4ff346b9d5203afbced0dbef3c28b6e1
Sandip Garai <sandip.garai.developer@gmail.com>
2018-08-08 23:00:31 +0530

    First Commit

---
+++ ExperimentApp1/src/main/java/com/sandip/ExperimentApp1/ExperimentApp1Application.java
@@ -0,0 +1,12 @@
+package com.sandip.ExperimentApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperimentApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExperimentApp1Application.class, args);
+	}
+}
